1db14c43210234e25788c54637da04fdefdc4acb
mrohith29 <mariyalarohith29@gmail.com>
2024-04-17 16:52:14 +0530

    add index

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def index1():
+    return render_template('index1.html')
 
 @app.route('/about')
 def about():
